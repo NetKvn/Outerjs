3683be87f3f382ba5df4821294f7ddb17676668f
Alan M.E <alanmarquez@outlook.com>
2020-08-31 22:51:00 -0500

    implemented StringBuilder

--- outer-core/src/stringBuilder.js
+++ outer-core/src/stringBuilder.js
@@ -1,3 +1,33 @@
-export default class StringBuilder extends String {
-    
+export default class StringBuilder {
+    constructor(sequence) {
+        this.arraySequence = [];
+        if (sequence instanceof Array)
+            this.arraySequence = [...sequence];
+        else if (sequence instanceof String)
+            this.arraySequence.push(sequence);
+        else
+            this.arraySequence.push(sequence.toString());
+    }
+    append(str) {
+        this.arraySequence.push(str);
+        return this;
+    }
+    insert(index, str) {
+        this.arraySequence.splice(index, 0, str);
+        return this;
+    }
+    reverse() {
+        return this.arraySequence = this.arraySequence.reverse();
+        return this;
+    }
+    clear() {
+        this.arraySequence.length = 0;
+        return this;
+    }
+    clone() {
+        return new StringBuilder(this.toString());
+    }
+    toString() {
+        return this.arraySequence.join("");
+    }
 }
